fbf201b07fd375785becbecb3040b19ecc2ab99c
vipor <sokvipor123@gmail.com>
2025-11-28 01:37:57 +0700

    new commit

--- EXERCISE-4/main.dart
+++ EXERCISE-4/main.dart
@@ -1,44 +1,46 @@
+// 1. What type of data will store the jokes? 
+// List of Maps (List<Map<String, String>>), the 'jokes' list from data/jokes.dart
+
+// 2. What type of data will store the favorite joke? 
+// int: stores the index of the favorite joke (_isFavIndex)
+
+// 3. Which widget should be in charge of storing the favorite joke?  
+// MyJoke widget it stores _isFavIndex which tracks which joke is the favorite
+
+// 4. Which widget should be stateful?
+// MyJoke should be stateful because it needs to remember which joke is favorited across rebuilds
+
+// 5. How will your widget interact? 
+// The child "FavoriteCard" calls a callback function "onFavoriteClick" when it was clicked so the parent updated the "_isFavIndex".
+
+// 6. Do you need to pass callback function between widgets?
+// YES, onFavorite callback is passed from MyJoke to FavoriteCard
+
 import 'package:flutter/material.dart';
+import 'data/jokes.dart';
 
 Color appColor = Colors.green[300] as Color;
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: appColor,
-          title: const Text("Favorite Jokes"),
-        ),
-        body: const Column(
-          children: [FavoriteCard(), FavoriteCard(), FavoriteCard()],
-        ),
-      ),
-    ));
+class FavoriteCard extends StatelessWidget {
+  final String title;
+  final String description;
+  final bool isFavorite;
+  final VoidCallback onFavorite;
 
-class FavoriteCard extends StatefulWidget {
   const FavoriteCard({
-    super.key,
-  });
-
-  @override
-  State<FavoriteCard> createState() => _FavoriteCardState();
-}
-
-class _FavoriteCardState extends State<FavoriteCard> {
-  bool _isFavorite = false;
-
-  void onFavoriteClick() {
-    setState(() {
-      _isFavorite = !_isFavorite;
+    super.key, 
+    required this.title, 
+    required this.description,
+    required this.isFavorite,
+    required this.onFavorite,
     });
-  }
- 
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
         border: Border(
-          bottom: BorderSide(width: .5, color: Colors.grey),
+          bottom: BorderSide(width: 0.5, color: Colors.grey),
         ),
       ),
       padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
@@ -49,25 +51,79 @@ class _FavoriteCardState extends State<FavoriteCard> {
             flex: 7,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
+
               children: [
                 Text(
-                  'title',
+                  title,
                   style: TextStyle(
                       color: appColor, fontWeight: FontWeight.w800),
                 ),
                 const SizedBox(height: 10.0),
-                const Text('description')
+
+                Text(description),
               ],
             ),
           ),
           IconButton(
-              onPressed: onFavoriteClick,
+              onPressed: onFavorite,
               icon: Icon(
-                _isFavorite ? Icons.favorite : Icons.favorite_border,
-                color: _isFavorite ? Colors.red : Colors.grey,
+                isFavorite ? Icons.favorite : Icons.favorite_border,
+                color: isFavorite ? Colors.red : Colors.grey,
               ))
         ],
       ),
     );
   }
+}
+class MyJoke extends StatefulWidget {
+  const MyJoke({
+    super.key,
+  });
+
+  @override
+  State<MyJoke> createState() => _MyJokeState();
+}
+
+class _MyJokeState extends State<MyJoke> {
+  int _isFavIndex = -1;
+
+  void onFav(int index) {
+    setState(() {
+      _isFavIndex = index;
+    });
+  }
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: appColor,
+          title: const Text("Favorite Jokes"),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: jokes.map((joke) {
+              int index = jokes.indexOf(joke);
+              return FavoriteCard(
+                title: joke['title']!,
+                description: joke['description']!,
+                isFavorite: _isFavIndex == index,
+                onFavorite: () {
+                  onFav(index);
+                },
+              );
+            }).toList(),
+          ),
+        ),
+      );
+  }
+}
+
+void main() { 
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyJoke(),
+    )
+  );
 }
